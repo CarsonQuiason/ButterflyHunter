e6ca5a8cf8d30f50259f274ed10661bd47b74ee1
mbb5ym <70290250+mbb5ym@users.noreply.github.com>
2021-12-05 18:27:44 -0600

    whirlwind

--- Assets/playerScript.cs
+++ Assets/playerScript.cs
@@ -6,11 +6,19 @@ public class playerScript : MonoBehaviour
 {
     private Vector3 _target;
     public Camera Camera;
+    public GameObject playerPref;
+    public bool whirlwind = false;
+
+    float t1 = 0f;
+    float t2 = 0f;
+    float duration = 1f;
+    Vector3 startScale;
+    Vector3 targetScale = Vector3.one * 2;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        startScale = playerPref.transform.localScale;
     }
 
     // Update is called once per frame
@@ -19,10 +27,42 @@ public class playerScript : MonoBehaviour
         _target = Camera.ScreenToWorldPoint(Input.mousePosition);
         _target.z = 0;
         transform.position = Vector3.MoveTowards(transform.position, _target, 1);
+
+        if(whirlwind)
+        {
+            Vector3 newZ = playerPref.transform.position;
+            newZ.z = -10;
+            playerPref.transform.position = newZ;
+            t1 += Time.deltaTime / duration;
+            Vector3 newScale = Vector3.Lerp(startScale, targetScale, t1);
+            playerPref.transform.localScale = newScale;
+            if(playerPref.transform.localScale == targetScale)
+            {
+                t2 += Time.deltaTime / duration;
+                newScale = Vector3.Lerp(targetScale, startScale, t2);
+                playerPref.transform.localScale = newScale;
+                if(playerPref.transform.localScale == startScale)
+                {
+                    t1 = t2 = 0f;
+                    newZ = playerPref.transform.position;
+                    newZ.z = 0;
+                    playerPref.transform.position = newZ;
+                    whirlwind = false;
+                }
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other);
+        switch(other.gameObject.tag)
+        {
+            case "whirlwind":
+                whirlwind = true;
+                break;
+            case "enemy":
+                Debug.Log("enemy");
+                break;
+        }
     }
 }
